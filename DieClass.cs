4779b7921ae927d4b52d2790003a09912e11a6f6
woods23 <woods23@GRANITE-5540-5>
2024-01-17 17:32:36 -0700

    Add project files.

---
+++ DieClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_2_Assignment
+{
+    internal class DieClass
+    {
+        public int [] RollDice(int NumOfRolls)
+        {
+            Random random = new Random();
+
+            int[] RollCombos = new int[11];
+
+            for (int i = 0; i < NumOfRolls; i++)
+            {
+                // Create an instance of the Random class
+                int Die1 = random.Next(1, 7);
+                int Die2 = random.Next(1, 7);
+                int DieTotal = Die1 + Die2;
+                RollCombos[DieTotal - 2]++;
+            }
+            return RollCombos;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Prompt the user for how many rolls and store the number to a variable and then parse it to an int
+
+using Mission_2_Assignment;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DieClass rolls = new DieClass();
+
+        Console.Write("How many times do you want to roll the dice? ");
+
+        string InputOfRolls = Console.ReadLine();
+
+        int NumOfRolls = int.Parse(InputOfRolls);
+
+        int[] RollResults = rolls.RollDice(NumOfRolls);
+
+        for (int i = 0; i < RollResults.Length; i++)
+        {
+            double percentage = (double)RollResults[i] / NumOfRolls * 100;
+            string stars = new string('*', (int)percentage);
+
+            Console.WriteLine($"{i + 2}: {percentage:F1}% {stars}");
+        }
+    }
+}
